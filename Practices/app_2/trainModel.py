860d65e013484186abd571203a65855406ad0653
itsFDavid <FDavid04@icloud.com>
2024-11-11 23:10:07 -0600

    refactor: clean up trainModel.py by removing unnecessary whitespace

--- Practices/app_2/trainModel.py
+++ Practices/app_2/trainModel.py
@@ -1,20 +1,14 @@
 from sklearn.linear_model import LogisticRegression
-from dataFramePreparer import DataFramePreparer
 from dataPreparer import dataPreparer, dataSetPreparer
 from sklearn.metrics import f1_score
 from divideDataset import dataSetTest
 
 X_test, y_test = dataSetTest()
 
-
 X_train_prep, X_val_prep, y_train = dataPreparer()
-
 
 clf = LogisticRegression(max_iter = 5000)
 clf.fit(X_train_prep, y_train)
-
-
-
 
 def predict():
     X_test_prep = dataSetPreparer(X_test)
