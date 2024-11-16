f13fc8af89615dc99ba035b8f09b5e598900ad91
itsFDavid <FDavid04@icloud.com>
2024-11-15 19:58:24 -0600

    feat: add precision and recall score endpoints in app.py and implement corresponding functions in trainModel.py

--- Practices/app_2/app.py
+++ Practices/app_2/app.py
@@ -1,16 +1,31 @@
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
-from trainModel import f1Score
+from trainModel import f1Score, precisionScore, recallScore
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/f1-Score", methods=["GET"])
 @cross_origin()
-def pred_anomaly():
+def predF1Score():
     f1_Score = f1Score()
     f1Score_rounded = round(f1_Score, 3)
     return jsonify({'f1Score': f1Score_rounded})
+
+@app.route("/precision-Score", methods=["GET"])
+@cross_origin()
+def predPrecisionScore():
+    precision_Score = precisionScore()
+    precisionScore_rounded = round(precision_Score, 3)
+    return jsonify({'precisionScore': precisionScore_rounded})
+
+@app.route("/recall-Score", methods=["GET"])
+@cross_origin()
+def predRecallScore():
+    recall_Score = recallScore()
+    recallScore_rounded = round(recall_Score, 3)
+    return jsonify({'recallScore': recallScore_rounded})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
--- Practices/app_2/trainModel.py
+++ Practices/app_2/trainModel.py
@@ -1,11 +1,13 @@
 from sklearn.linear_model import LogisticRegression
-from dataPreparer import dataPreparer, dataSetPreparer
+from dataPreparer import dataPreparer, dataSetPreparer, dataSetVal
 from sklearn.metrics import f1_score
 from divideDataset import dataSetTest
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 X_test, y_test = dataSetTest()
-
 X_train_prep, X_val_prep, y_train = dataPreparer()
+X_val, y_val = dataSetVal()
 
 clf = LogisticRegression(max_iter = 5000)
 clf.fit(X_train_prep, y_train)
@@ -15,3 +17,10 @@ def f1Score():
     y_pred = clf.predict(X_test_prep)
     f1Score = f1_score(y_test, y_pred, pos_label = 'anomaly')
     return f1Score
+
+y_pred = clf.predict(X_val_prep)
+def precisionScore():
+    return precision_score(y_val, y_pred, pos_label = 'anomaly')
+
+def recallScore():
+    return recall_score(y_val, y_pred, pos_label = 'anomaly')
