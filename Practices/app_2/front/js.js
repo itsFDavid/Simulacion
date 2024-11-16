1c0ad2f8fb71c9dfcb1989aa58a868a3ca0fc763
itsFDavid <FDavid04@icloud.com>
2024-11-15 19:58:47 -0600

    feat: update prediction button to fetch F1 score, precision, and recall, and display results in the UI

--- Practices/app_2/front/js.js
+++ Practices/app_2/front/js.js
@@ -1,22 +1,50 @@
-const btnPredict = document.getElementById("predict")
-btnPredict.addEventListener("click", getF1Score)
+const btnPredict = document.getElementById("predict");
+btnPredict.addEventListener("click", (event) => {
+  getF1Score(event);
+  getPrecision(event);
+  getRecall(event);
+});
 
 async function getF1Score(event) {
-    event.preventDefault()
-    const URL = "http://localhost:4000/anomaly-predict"
-    const response = await fetch(URL)
-    const data = await response.json()
-    console.log(data)
-    const f1Score = data.f1Score 
-    const f1ScoreElement = document.getElementById("txt-anomaly")
-    f1ScoreElement.innerHTML = f1Score
-    const matrix = document.getElementById("matrix")
-    matrix.style.display = "flex"
+  event.preventDefault();
+  const URL = "http://localhost:4000/f1-Score";
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+  const f1Score = data.f1Score;
+  const f1ScoreElement = document.getElementById("txt-anomaly");
+  f1ScoreElement.innerHTML = f1Score;
+  const matrix = document.getElementById("matrix");
+  matrix.style.display = "grid";
+}
+async function getPrecision(event) {
+  event.preventDefault();
+  const URL = "http://localhost:4000/precision-Score";
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+  const precision = data.precisionScore;
+  const precisionElement = document.getElementById("txt-precision");
+  precisionElement.innerHTML = precision;
+}
+async function getRecall(event) {
+  event.preventDefault();
+  const URL = "http://localhost:4000/recall-Score";
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+  const recall = data.recallScore;
+  const recallElement = document.getElementById("txt-recall");
+  recallElement.innerHTML = recall;
 }
 
 document.addEventListener("reset", () => {
-    const f1ScoreElement = document.getElementById("txt-anomaly")
-    f1ScoreElement.innerHTML = ""
-    const matrix = document.getElementById("matrix")
-    matrix.style.display = "none"
+  const f1ScoreElement = document.getElementById("txt-anomaly");
+  f1ScoreElement.innerHTML = "";
+  const matrix = document.getElementById("matrix");
+  matrix.style.display = "none";
+  const precisionElement = document.getElementById("txt-precision");
+  precisionElement.innerHTML = "";
+  const recallElement = document.getElementById("txt-recall");
+  recallElement.innerHTML = "";
 });
